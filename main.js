0b8726b624345e967ca36b3cfa656dccff9f104c
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2024-04-30 07:44:38 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-console.log(textos)
+
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -15,7 +15,4 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document. querySelectorAll (".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].TextoContent = tempoobjetivo1 - tempoAtual
+contadores[0].TextoContent =  "contagem regressiva"
