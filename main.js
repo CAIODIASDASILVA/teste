f8243c6de39d4d7b16563d6e77c1da5744c16e36
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2024-05-14 07:56:45 -0300

    ,

--- main.js
+++ main.js
@@ -16,6 +16,13 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document. querySelectorAll (".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00:00:00")
-let tempoAtual = new Date()
+  
 
 contadores[0].textContent =  (tempoObjetivo1 - tempoAtual)/1000/60/24/60
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    return segundos;
+  }
